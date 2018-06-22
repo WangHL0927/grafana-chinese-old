7396e11bd9ca2d27ca115b0a3e42ed8229db1634
Wang Hualong <whl958660278@gmail.com>
2018-06-22 12:53:55 +0800

    fix bug

--- public/build/dark.a2a290aabf5c9d0da40c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{1651:function(n,c){}},[1651]);
-//# sourceMappingURL=dark.a2a290aabf5c9d0da40c.js.map
--- public/build/light.a2a290aabf5c9d0da40c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{1652:function(n,c){}},[1652]);
-//# sourceMappingURL=light.a2a290aabf5c9d0da40c.js.map
